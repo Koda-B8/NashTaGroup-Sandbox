7349b22b4e8352567bbab2cdffb0d36bf253cad4
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-09-02 22:42:24 +0700

    chore: setup git hooks

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import oxfmt from "@kekkon-nexus/config/oxfmt";
+import oxlint from "@kekkon-nexus/config/oxlint";
+import react from "@kekkon-nexus/config/oxlint/react";
+import vp from "@kekkon-nexus/config/oxlint/vite-plus";
+import { defineConfig } from "vite-plus";
+
+export default defineConfig({
+	fmt: { ...oxfmt },
+	lint: {
+		extends: [oxlint, react, vp],
+	},
+	staged: {
+		"*": "vp check --no-error-on-unmatched-pattern --fix",
+	},
+});

1b7b3986add9e8020eb31ceda1a0b578be484d66
Muhamad Rafli Shidiq <muhamadraflishidiq@gmail.com>
2026-09-06 23:22:19 +0700

    feat(db): add roles migration

---
+++ apps/backend/migrations/20260905000100-create-roles.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-undef, unicorn/prefer-module */
+
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable("roles", {
+			id: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+			},
+			name: { type: Sequelize.STRING(50), allowNull: false, unique: true },
+			created_at: {
+				type: Sequelize.DATE,
+				allowNull: false,
+				defaultValue: Sequelize.fn("NOW"),
+			},
+			updated_at: {
+				type: Sequelize.DATE,
+				allowNull: false,
+				defaultValue: Sequelize.fn("NOW"),
+			},
+			deleted_at: { type: Sequelize.DATE, allowNull: true },
+		});
+	},
+
+	async down(queryInterface) {
+		await queryInterface.dropTable("roles");
+	},
+};

13e8b30cbabcb2832967fae31c05d31d8d688e26
Muhamad Rafli Shidiq <muhamadraflishidiq@gmail.com>
2026-09-07 11:27:49 +0700

    refactor(db): use UUID v4 for inventory IDs

--- apps/backend/migrations/20260905000600-create-inventories.js
+++ apps/backend/migrations/20260905000600-create-inventories.js
@@ -4,13 +4,13 @@ module.exports = {
 	async up(queryInterface, Sequelize) {
 		await queryInterface.createTable("inventories", {
 			id: {
-				type: Sequelize.INTEGER,
+				type: Sequelize.UUID,
 				allowNull: false,
-				autoIncrement: true,
+				defaultValue: Sequelize.literal("gen_random_uuid()"),
 				primaryKey: true,
 			},
 			product_item_id: {
-				type: Sequelize.INTEGER,
+				type: Sequelize.UUID,
 				allowNull: false,
 				unique: true,
 				references: { model: "product_items", key: "id" },

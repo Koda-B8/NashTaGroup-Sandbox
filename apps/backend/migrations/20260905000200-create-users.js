e0637f785fbb75c4346e0411c87263aed73e85ae
Muhamad Rafli Shidiq <muhamadraflishidiq@gmail.com>
2026-09-07 11:27:47 +0700

    refactor(db): use UUID v4 for user IDs

--- apps/backend/migrations/20260905000200-create-users.js
+++ apps/backend/migrations/20260905000200-create-users.js
@@ -4,13 +4,13 @@ module.exports = {
 	async up(queryInterface, Sequelize) {
 		await queryInterface.createTable("users", {
 			id: {
-				type: Sequelize.INTEGER,
+				type: Sequelize.UUID,
 				allowNull: false,
-				autoIncrement: true,
+				defaultValue: Sequelize.literal("gen_random_uuid()"),
 				primaryKey: true,
 			},
 			role_id: {
-				type: Sequelize.INTEGER,
+				type: Sequelize.UUID,
 				allowNull: false,
 				references: { model: "roles", key: "id" },
 				onUpdate: "CASCADE",

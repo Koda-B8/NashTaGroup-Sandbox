cfda8f3f5a3fe7d41267e71fb34478f6e8af928b
Muhammad Wahyu Pratama <muhammad123wahyu123@gmail.com>
2026-09-10 11:20:44 +0700

    feat(product): add product management routes and swagger docs

--- apps/backend/routes/index.js
+++ apps/backend/routes/index.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
 
 import authRoute from "./AuthRoute.js";
+import brandRoute from "./BrandRoute.js";
 import categoryRoute from "./CategoryRoute.js";
+import productItemRoute from "./ProductItemRoute.js";
+import productRoute from "./ProductRoute.js";
 
 const router = Router();
 
 router.use("/auth", authRoute);
 router.use("/categories", categoryRoute);
+router.use("/brands", brandRoute);
+router.use("/products", productRoute);
+router.use("/product-items", productItemRoute);
 
 export default router;

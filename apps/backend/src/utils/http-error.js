96c15f9d29a408f022349cd9aeba9b2ed91b047e
Muhammad Wahyu Pratama <muhammad123wahyu123@gmail.com>
2026-09-15 14:34:03 +0700

    feat(checkout): add atomic checkout controller and tests

--- apps/backend/src/utils/http-error.js
+++ apps/backend/src/utils/http-error.js
@@ -1,9 +1,13 @@
 export class HttpError extends Error {
-	constructor(statusCode, message) {
+	constructor(statusCode, message, errors) {
 		super(message);
 		this.statusCode = statusCode;
+
+		if (errors !== undefined) {
+			this.errors = errors;
+		}
 	}
 }
 
-export const createHttpError = (statusCode, message) =>
-	new HttpError(statusCode, message);
+export const createHttpError = (statusCode, message, errors) =>
+	new HttpError(statusCode, message, errors);
